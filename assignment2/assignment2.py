b102bc6ea10ec7b4d468ce9771731861379120a7
Angel <stetchi.aigul@gmail.com>
2025-03-24 11:00:02 -0400

    task 7

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -30,3 +30,33 @@ def column_index(column_name):
         
 employee_id_column = column_index("employee_id")
 print(employee_id_column)
+
+# Task 4: Find the Employee First Name
+def first_name(row_number):
+    first_name_ind = column_index("first_name")
+    row = employees['rows'][row_number]
+    value = row[first_name_ind]
+    return value
+print(employees)
+
+# Task 5: Find the Employee: a Function in a Function
+def employee_find(employee_id):
+    def employee_match(row):
+            return int(row[employee_id_column]) == employee_id
+    matches=list(filter(employee_match, employees["rows"]))
+    return matches
+# Task 6: Find the Employee with a Lambda
+def employee_find_2(employee_id):
+     matches = list(filter(lambda row : int(row[employee_id_column]) == employee_id , employees["rows"]))
+     return matches
+# result = employee_find_2(3)  
+# print(result)
+# Task 7: Sort the Rows by last_name Using a Lambda
+def sort_by_last_name():
+     last_name_ind = column_index("last_name")
+    #  print("Before sorting:", employees["rows"])
+     employees["rows"].sort(key = lambda x: x[last_name_ind])
+    #  print("After sorting:", employees["rows"])
+     return employees["rows"] 
+sorted_rows = sort_by_last_name()
+print(employees)
